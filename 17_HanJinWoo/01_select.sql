61723e5ed7715c2904ff55530de67a57fa4300b7
harukiki97 <qwejinoohan@gmail.com>
2025-11-03 14:20:15 +0900

    swcamp 계정생성, DB_SCRIPT, 01_SELECT 진행중

---
+++ 17_HanJinWoo/01_select.sql
@@ -0,0 +1,36 @@
+-- 01_select : 특정 테이블의 원하는 데이터(행)을 조회하는 구문
+
+-- tbl_menu 테이블의 모든 메뉴 이름(menu_name) 조회
+/*범위 주석 (단축키 ctrl + shift + /) */
+/*2*/
+select
+    menu_name
+/*1*/
+from
+    tbl_menu;
+
+-- 모든 메뉴의 menu_code, menu_name, menu_price 조회
+select
+        menu_code,
+        menu_name,
+        menu_price
+
+from
+    tbl_menu;
+
+-- tbl_menu 테이블 모든 열 조회
+select
+    menu_code,
+    menu_name,
+    menu_price,
+    category_code,
+    orderable_status
+
+
+FROM
+    tbl_menu;
+
+select
+    *
+from
+    tbl_menu;
